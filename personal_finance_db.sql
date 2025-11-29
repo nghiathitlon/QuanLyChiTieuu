d400c0700bd095f8897ff5e7ed434b7f8ebb5beb
VOLENGOCTHUAN <thuan.vln.64cntt@ntu.edu.vn>
2025-11-29 17:48:03 +0700

    Update personal_finance_db.sql

--- personal_finance_db.sql
+++ personal_finance_db.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Nov 24, 2025 at 01:19 PM
+-- Generation Time: Nov 29, 2025 at 11:47 AM
 -- Server version: 10.4.32-MariaDB
 -- PHP Version: 8.2.12
 
@@ -59,7 +59,7 @@ CREATE TABLE `budget` (
 --
 
 INSERT INTO `budget` (`id`, `user_id`, `month`, `year`, `amount`, `created_at`, `updated_at`) VALUES
-(1, 5, 11, 2025, 5000000.00, '2025-11-19 17:38:09', '2025-11-22 19:34:58'),
+(1, 5, 11, 2025, 7000000.00, '2025-11-19 17:38:09', '2025-11-25 09:31:58'),
 (2, 5, 3, 2025, 3000000.00, '2025-11-20 07:56:46', '2025-11-20 19:36:35'),
 (5, 4, 4, 2025, 0.00, '2025-11-20 18:36:16', '2025-11-20 18:36:16'),
 (7, 5, 2, 2023, 5000000.00, '2025-11-23 07:42:49', '2025-11-23 07:42:49');
@@ -94,8 +94,10 @@ INSERT INTO `categories` (`category_id`, `user_id`, `name`, `type`) VALUES
 (22, 4, 'an chieu', 'expense'),
 (24, 5, 'di choi', 'expense'),
 (25, 5, 'ban xe', 'income'),
-(26, 5, 'an toi', 'expense'),
-(29, 5, 'mua do ve sinh du phong', 'expense');
+(29, 5, 'mua do ve sinh du phong', 'expense'),
+(33, 5, 'bán đồ chơi', 'expense'),
+(35, 5, 'bán bánh mì', 'income'),
+(36, 5, 'bán bánh mì sandwich', 'expense');
 
 -- --------------------------------------------------------
 
@@ -139,8 +141,31 @@ INSERT INTO `reminders` (`id`, `user_id`, `title`, `description`, `remind_date`,
 (3, 5, 'Đóng tiền quỹ lớp', '90.000 VNĐ', '2025-11-25', 1, '2025-11-22 16:41:46', NULL),
 (4, 5, 'Đóng tiền hụi cho anh 3 chợ gà', '790.000 VNĐ', '2025-11-27', 0, '2025-11-22 16:42:19', NULL),
 (6, 5, 'Đóng tiền ủng hộ mùa lũ 2025', '6.666.000VNĐ', '2025-11-24', 0, '2025-11-22 16:43:52', NULL),
-(7, 5, 'đóng học phí', '50.00.000', '2025-11-21', 1, '2025-11-22 19:49:31', NULL),
-(9, 5, 'học thêm', '200.000 VNĐ', '2025-11-23', 0, '2025-11-22 19:54:05', NULL);
+(7, 5, 'đóng học phí cho con trai', '5.00.000', '2025-11-22', 1, '2025-11-22 19:49:31', NULL),
+(9, 5, 'học thêm', '200.000 VNĐ', '2025-11-23', 1, '2025-11-22 19:54:05', NULL);
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `savings`
+--
+
+CREATE TABLE `savings` (
+  `id` int(11) NOT NULL,
+  `user_id` int(11) NOT NULL,
+  `name` varchar(255) NOT NULL,
+  `amount` bigint(20) NOT NULL,
+  `created_at` datetime DEFAULT current_timestamp()
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_general_ci;
+
+--
+-- Dumping data for table `savings`
+--
+
+INSERT INTO `savings` (`id`, `user_id`, `name`, `amount`, `created_at`) VALUES
+(9, 5, 'Tiết kiệm tháng 11/2025', 790000, '2025-11-29 16:09:57'),
+(10, 5, 'Tiết kiệm tháng 11/2025', 110000, '2025-11-29 16:17:03'),
+(12, 5, 'Tiết kiệm tháng 11/2025', 100000, '2025-11-29 16:38:33');
 
 -- --------------------------------------------------------
 
@@ -174,12 +199,13 @@ INSERT INTO `transactions` (`transaction_id`, `user_id`, `type`, `category_id`, 
 (17, 4, NULL, 20, NULL, 200000.00, '2025-11-19', '', '2025-11-19 11:45:04'),
 (18, 4, NULL, 22, NULL, 199000.00, '2025-12-11', '', '2025-11-19 11:45:53'),
 (19, 4, NULL, 21, NULL, 199000.00, '2025-11-12', '', '2025-11-19 11:46:20'),
-(37, 5, NULL, 25, NULL, 10000000.00, '2025-11-01', 'lanh luong', '2025-11-19 17:55:09'),
-(39, 5, NULL, 24, NULL, 1800000.00, '2025-11-24', '0', '2025-11-19 17:55:52'),
+(37, 5, NULL, 25, NULL, 10000000.00, '2025-11-01', '0', '2025-11-19 17:55:09'),
+(39, 5, NULL, 24, NULL, 800000.00, '2025-11-24', '222', '2025-11-19 17:55:52'),
 (42, 5, NULL, 24, NULL, 5000000.00, '2025-11-22', '0', '2025-11-19 17:57:55'),
 (44, 5, NULL, 25, NULL, 3000000.00, '2025-11-05', '0', '2025-11-20 06:52:26'),
 (45, 5, NULL, 25, NULL, 1000000.00, '2025-11-05', '0', '2025-11-20 07:14:47'),
-(47, 5, NULL, 29, NULL, 300000.00, '2025-11-25', '13311558', '2025-11-22 19:44:45');
+(47, 5, NULL, 29, NULL, 300000.00, '2025-11-25', '0', '2025-11-22 19:44:45'),
+(50, 5, NULL, 25, NULL, 1000000.00, '2025-11-25', 'ban 10 o banh mi ', '2025-11-29 10:21:07');
 
 -- --------------------------------------------------------
 
@@ -192,19 +218,22 @@ CREATE TABLE `users` (
   `email` varchar(255) NOT NULL,
   `username` varchar(100) NOT NULL,
   `password_hash` varchar(255) NOT NULL,
-  `created_at` timestamp NOT NULL DEFAULT current_timestamp()
+  `created_at` timestamp NOT NULL DEFAULT current_timestamp(),
+  `avatar` varchar(255) DEFAULT NULL,
+  `reset_token` varchar(255) DEFAULT NULL,
+  `reset_expire` datetime DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 --
 -- Dumping data for table `users`
 --
 
-INSERT INTO `users` (`user_id`, `email`, `username`, `password_hash`, `created_at`) VALUES
-(1, 'jamesmoccua159@gmail.com', '64131472', '$2y$10$yswltJrLlbAMBFLhC9WT..CZ1/ohAwS21uhVKHir8kVNDSYXi0KJC', '2025-11-14 06:38:36'),
-(2, '123@gmail.com', '123', '$2y$10$wp/.7Y8c13v8B3V8ug/rB.GigYbqatezXCrUTQvNeIPIzpJ4.GkVC', '2025-11-14 08:03:29'),
-(3, 'teobuong89@gmail.com', 'Tèo', '$2y$10$Qtbqn0PaaG7tJy9iGeP.VuTHY7S.KKGlC/CzWInnSXzWxTty6vhWe', '2025-11-14 08:30:35'),
-(4, 'orionkid93@gmail.com', '64132410', '$2y$10$XTK56.Hm0QBkGCY7lLGxiuE4OtlerX3oP7VhAVVa5A0FyYJt4x/XG', '2025-11-19 10:45:10'),
-(5, 'thuan.vln.64cntt@ntu.edu.vn', 'Thuann ne', '$2y$10$4mwvM1RXw/vhTdf6G3NF4e9mMfARRHok835t2Bxptv6hEQ5dp7ySK', '2025-11-19 14:04:09');
+INSERT INTO `users` (`user_id`, `email`, `username`, `password_hash`, `created_at`, `avatar`, `reset_token`, `reset_expire`) VALUES
+(1, 'jamesmoccua159@gmail.com', '64131472', '$2y$10$yswltJrLlbAMBFLhC9WT..CZ1/ohAwS21uhVKHir8kVNDSYXi0KJC', '2025-11-14 06:38:36', NULL, NULL, NULL),
+(2, '123@gmail.com', '123', '$2y$10$wp/.7Y8c13v8B3V8ug/rB.GigYbqatezXCrUTQvNeIPIzpJ4.GkVC', '2025-11-14 08:03:29', NULL, NULL, NULL),
+(3, 'teobuong89@gmail.com', 'Tèo', '$2y$10$Qtbqn0PaaG7tJy9iGeP.VuTHY7S.KKGlC/CzWInnSXzWxTty6vhWe', '2025-11-14 08:30:35', NULL, NULL, NULL),
+(4, 'orionkid93@gmail.com', '64132410', '$2y$10$XTK56.Hm0QBkGCY7lLGxiuE4OtlerX3oP7VhAVVa5A0FyYJt4x/XG', '2025-11-19 10:45:10', NULL, NULL, NULL),
+(5, 'thuan.vln.64cntt@ntu.edu.vn', 'Thuann ne', '$2y$10$8nPVNKrRrxxC/Md0uLKBUORQcSIkm1.FefUW80XAKB1lsfquCvyZy', '2025-11-19 14:04:09', '1763995766_meme-meo.webp', '8f973c858f138f792a568f15a37ab5a2', '2025-11-29 09:51:21');
 
 --
 -- Indexes for dumped tables
@@ -246,6 +275,12 @@ ALTER TABLE `reminders`
   ADD KEY `user_id` (`user_id`);
 
 --
+-- Indexes for table `savings`
+--
+ALTER TABLE `savings`
+  ADD PRIMARY KEY (`id`);
+
+--
 -- Indexes for table `transactions`
 --
 ALTER TABLE `transactions`
@@ -280,7 +315,7 @@ ALTER TABLE `budget`
 -- AUTO_INCREMENT for table `categories`
 --
 ALTER TABLE `categories`
-  MODIFY `category_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=32;
+  MODIFY `category_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=38;
 
 --
 -- AUTO_INCREMENT for table `monthlybudget`
@@ -295,10 +330,16 @@ ALTER TABLE `reminders`
   MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=10;
 
 --
+-- AUTO_INCREMENT for table `savings`
+--
+ALTER TABLE `savings`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=13;
+
+--
 -- AUTO_INCREMENT for table `transactions`
 --
 ALTER TABLE `transactions`
-  MODIFY `transaction_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=48;
+  MODIFY `transaction_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=52;
 
 --
 -- AUTO_INCREMENT for table `users`
