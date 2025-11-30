f82162d0834ebda99eaf23fa472f9d25479faf95
justNghia <121323644+nghiathitlon@users.noreply.github.com>
2025-11-30 14:09:04 +0700

    update CSS

--- login.php
+++ login.php
@@ -1,36 +1,45 @@
 <!DOCTYPE html>
 <html>
+<?php require_once 'header.php'?>
 
 <head>
     <title>Đăng nhập</title>
 </head>
+<style>
+input {
+    display: block;
+    margin-bottom: 10px;
+    padding: 8px;
+    width: 300px;
+    box-sizing: border-box;
+}
+</style>
+
 
 <body>
-    <?php require 'header.php'; ?>
-    <h2>Đăng nhập</h2>
 
+    <h2>Đăng nhập</h2>
     <?php
         if (isset($_GET['register']) && $_GET['register'] == 'success') {
             echo "<p style='color:green;'>Đăng ký thành công! Vui lòng đăng nhập.</p>";
         }
     ?>
+    <div class="m-container">
+        <div class="form-main-container">
+            <form action="actions/action_login.php" method="POST">
+                <label>Email:</label>
+                <input type="email" name="email" required>
+                <label>Mật khẩu:</label>
+                <input type="password" name="password" required>
+                <button type="submit">Đăng nhập</button>
+            </form>
+            <!-- Quên mật khẩu -->
+            <p><a href="forgot_password.php">Quên mật khẩu?</a></p>
+            <p>Chưa có tài khoản? <a href="register.php">Đăng ký</a></p>
+            <?php require 'footer.php'; ?>
+        </div>
+    </div>
 
-    <form action="actions/action_login.php" method="POST">
-        <label>Email:</label>
-        <input type="email" name="email" required>
-
-        <label>Mật khẩu:</label>
-        <input type="password" name="password" required>
-
-        <button type="submit">Đăng nhập</button>
-    </form>
-
-    <!-- Quên mật khẩu -->
-    <p><a href="forgot_password.php">Quên mật khẩu?</a></p>
-
-    <p>Chưa có tài khoản? <a href="register.php">Đăng ký</a></p>
-
-    <?php require 'footer.php'; ?>
 </body>
 
 </html>
--- register.php
+++ register.php
@@ -6,23 +6,37 @@
     <title>Đăng ký</title>
     <link rel="stylesheet" href="css/style.css">
 </head>
+<style>
+input {
+    display: block;
+    margin-bottom: 10px;
+    padding: 8px;
+    width: 300px;
+    ;
+
+}
+</style>
 
 <body>
-    <h2>Đăng ký tài khoản</h2>
-    <form action="actions/action_register.php" method="POST">
-        <label>Tên hiển thị:</label>
-        <input type="text" name="username" required>
+    <div class="container">
+        <div classs="form-main-container">
+            <h2>Đăng ký tài khoản</h2>
+            <form action="actions/action_register.php" method="POST">
+                <label>Tên hiển thị:</label>
+                <input type="text" name="username" required>
 
-        <label>Email:</label>
-        <input type="email" name="email" required>
+                <label>Email:</label>
+                <input type="email" name="email" required>
 
-        <label>Mật khẩu:</label>
-        <input type="password" name="password" required>
+                <label>Mật khẩu:</label>
+                <input type="password" name="password" required>
 
-        <button type="submit">Đăng ký</button>
-    </form>
-    <p>Đã có tài khoản? <a href="login.php">Đăng nhập ngay</a></p>
-    <?php require 'footer.php'; ?>
+                <button type="submit">Đăng ký</button>
+            </form>
+            <p>Đã có tài khoản? <a href="login.php">Đăng nhập ngay</a></p>
+            <?php require 'footer.php'; ?>
+        </div>
+    </div>
 </body>
 
 </html>
