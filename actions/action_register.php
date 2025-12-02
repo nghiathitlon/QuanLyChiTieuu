13faa8ba233c73bf8bfda0f506a196f5aa9a2439
NGUYENHUUTHO12122004 <tho.nh.64cntt@ntu.edu.vn>
2025-12-02 09:08:09 +0700

    Update action_register.php

--- actions/action_register.php
+++ actions/action_register.php
@@ -9,7 +9,7 @@ $password = $_POST['password'];
 $password_hash = password_hash($password, PASSWORD_DEFAULT);
 
 // 3. Chuẩn bị câu lệnh SQL (Dùng prepared statements để chống SQL Injection)
-$stmt = $conn->prepare("INSERT INTO Users (username, email, password_hash) VALUES (?, ?, ?)");
+$stmt = $conn->prepare("INSERT INTO Users (username, email, password) VALUES (?, ?, ?)");
 $stmt->bind_param("sss", $username, $email, $password_hash);
 
 // 4. Thực thi và kiểm tra
